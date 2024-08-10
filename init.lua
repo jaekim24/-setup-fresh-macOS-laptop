4082865721e45b78402fcc665a82e9d494f7d500
gabriel horne <gabriel@gabriels-MacBook-Pro.local>
2024-08-10 01:23:42 -0400

    updated

--- init.lua
+++ init.lua
@@ -41,4 +41,19 @@ hs.hotkey.bind('ctrl', 't', function () hs.application.launchOrFocus("Terminal")
 hs.hotkey.bind('ctrl', 'b', function () hs.application.launchOrFocus("Brave Browser Beta") end)
 hs.hotkey.bind('ctrl', 'f', function () hs.application.launchOrFocus("Finder") end)
 
---hs.reload()
+
+-- automatically reload configs
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
